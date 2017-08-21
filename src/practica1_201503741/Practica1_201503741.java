9c8eeabd7b856861162703474a9bdd7dd6ef7542
Eddy Alejandro <Eddy Alejandro@EddyMurga>
2017-08-21 00:42:16 -0600

    cambio de nombre a la clase principiante a clase Jugar_201503741 creacion de metodos para las matrices y metodos para voltear y opciones de juego

--- src/practica1_201503741/Practica1_201503741.java
+++ src/practica1_201503741/Practica1_201503741.java
@@ -20,28 +20,38 @@ public class Practica1_201503741 {
         menu();
     }
 
-    private static void menu() {
-        System.out.println("EDDY ALEJANDRO MURGA BARILLAS");
-        System.out.println("         201503741");
-        System.out.println("      IPC A Practica 1");
-        System.out.println("       BUSCAMINAS!!!!");
+    public static void menu() {
+        int n=0;
+        System.out.println("          EDDY ALEJANDRO MURGA BARILLAS");
+        System.out.println("                   201503741");
+        System.out.println("               IPC A Practica 1");
+        System.out.println("                 BUSCAMINAS!!!!");
         Scanner enter = new Scanner(System.in);
         String presionar= enter.nextLine();
-        System.out.println("------------------------------");
-        System.out.println("         BUSCAMINAS!!!        ");
-        System.out.println("       1. Principiante        ");
-        System.out.println("       2. Intermedio          ");
-        System.out.println("       3. Experto             ");
-        System.out.println("       4. Salir               ");
+        System.out.println("--------------------------------------------------");
+        System.out.println("                   BUSCAMINAS!!!                  ");
+        System.out.println("                 1. Principiante                  ");
+        System.out.println("                 2. Intermedio                    ");
+        System.out.println("                 3. Experto                       ");
+        System.out.println("                 4. Salir                         ");
         System.out.println("Ingrese Opcion: ");
         Scanner p1Option= new Scanner(System.in);
         int op = p1Option.nextInt();
-        System.out.println("------------------------------");
+        System.out.println("--------------------------------------------------");
         
         switch(op){
             case 1:
+                n=4;
+                new Jugar_201503741().crear(n);
+                break;
             case 2:
+                n=6;
+                new Jugar_201503741().crear(n);
+                break;
             case 3:
+                n=8;
+                new Jugar_201503741().crear(n);
+                break;
             case 4:
                 break;
         }
